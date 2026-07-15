26ee7cd6727961e07f4a273aac8f1daf8bc090d1
KR$NA <78905769+KrishnaTiwariCoder@users.noreply.github.com>
2026-07-15 07:11:17 +0530

    chore: update

--- algo.md
+++
@@ -1,5 +0,0 @@
-
-1.sum=0, traverse through the string, create checked_string, and update this one by one
-2. at each element, an inner loop
-3. inner loops checks that the checked_string does contain one a one b one c;
-4. if checked_string = true, then update sum+=1
--- main.cpp
+++ main.cpp
@@ -2,68 +2,15 @@
 
 using namespace std;
 
-
-// class Solution {
-// public:
-//     bool isValid(string s) {
-//         stack<char> stack;
-//         for(int i = 0; i < s.length() ; i++ ){
-//             char element = s[i];
-//             if(string("([{").find(element)!= string::npos){
-//                 stack.push(element);
-//             } else {
-//                 if(stack.empty()) return 0;
-//                 switch (element){
-//                 case ')':
-//                     if(stack.top()=='(') stack.pop();
-//                     else return 0;
-//                     break;
-//                 case ']':
-//                     if(stack.top()=='[') stack.pop();
-//                     else return 0;
-//                     break;
-//                 case '}':
-//                     if(stack.top()=='{') stack.pop();
-//                     else return 0;
-//                     break;
-//                 default:
-//                     break;
-//                 }
-
-//             }
-//         }
-//         return stack.empty();
-//     }
-// };
-
-class Solution {
-public:
-    int numberOfSubstrings(string s) {
-        int sum = 0;
-        cout<<s<< s.length() << endl;
-        for (int i =0; i<s.length(); i++){
-            // string checked_string =s[i]+"";
-            for (int j=i; j<s.length()-1; j++){
-                // checked_string = checked_string + s[i+j];
-                // cout << "i: " << i << " j: " << j << endl;
-                cout << j << endl;
-            }
-            
-            cout << "\n\n-------------------" << endl;
-            cout << i << endl;
-            cout << "-------------------\n\n" << endl;
-        }
-        cout << "\n\n\n";
-        cout << "--------- THE RESULT ----------------" << "\n\n"; 
-        return sum;
+vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
+    for(auto it = queries.begin() ; it != queries.end(); it++ ){
+        cout << it << endl;
     }
-};
-
+}
 
 int main(){
-    Solution s;
-    string given;
-    cin>>given;
-    cout << s.numberOfSubstrings(given);
-    return -1;
+    string input;
+    cin >> input;
+
+    return 0;
 }
